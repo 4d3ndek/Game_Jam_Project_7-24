5ed0418178750f1ef77fd18e55aa9806554ad910
doome <doomer768@gmail.com>
2024-10-29 15:12:05 -0500

    new account contribution

--- InventorySlot.gd
+++ InventorySlot.gd
@@ -16,7 +16,7 @@ func _can_drop_data(_at_position: Vector2, data: Variant) -> bool:
 
 
 # _at_position is not used because it doesn't matter where on the panel
-# the item is dropped
+# the item is dropped 
 func _drop_data(_at_position: Vector2, data: Variant) -> void:
 	if get_child_count() > 0:
 		var item := get_child(0)
@@ -25,3 +25,6 @@ func _drop_data(_at_position: Vector2, data: Variant) -> void:
 		data.get_parent().add_child(item)
 	data.get_parent().remove_child(data)
 	add_child(data)
+
+
+# test
